dbb02f7b6127f898a9b1bf018f7e85f44768585b
Mtolu <98477256+Mtolu@users.noreply.github.com>
2026-07-01 18:43:48 +0200

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,28 @@
+const announces = [
+    "DIGIHUB ACADEMY", 
+    "★", 
+    "React Assignment", 
+    "★"
+];
+
 function Header() {
+    const repeatedHeads = [...announces, ...announces, ...announces, ...announces, ...announces];
+    
     return (
         <div className="header">
+             <div className="marquee-container">
+                <div className="marquee-track">
+                    {repeatedHeads.map((announce, index)=>(
+                        <span key={`${announce}-${index}`}>{announce}</span>
+                    ))}
+                </div>
+
+                <div className="marquee-track">
+                    {repeatedHeads.map((announce, index)=>(
+                        <span key={`${announce}-${index}`}>{announce}</span>
+                    ))}
+                </div>
+            </div> 
             <h1>Mini Blog Explorer</h1>
             <h2>React State + useEffect + Public API </h2>
             <h3>The Total Posts Counts: 100 </h3>
