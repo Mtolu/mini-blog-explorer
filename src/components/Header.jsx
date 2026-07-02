c5286b5ab96f7eb1533f22a377ad2e667875220c
Mtolu <98477256+Mtolu@users.noreply.github.com>
2026-07-02 02:52:12 +0200

    Fix header structure and maintain marquee functionality

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -9,23 +9,25 @@ function Header() {
     const repeatedHeads = [...announces, ...announces, ...announces, ...announces, ...announces];
     
     return (
-        <div className="header">
+        <div className="">
              <div className="marquee-container">
                 <div className="marquee-track">
                     {repeatedHeads.map((announce, index)=>(
                         <span key={`${announce}-${index}`}>{announce}</span>
                     ))}
                 </div>
-
+                
                 <div className="marquee-track">
                     {repeatedHeads.map((announce, index)=>(
                         <span key={`${announce}-${index}`}>{announce}</span>
                     ))}
                 </div>
-            </div> 
+            </div>
+            <div className="header">
             <h1>Mini Blog Explorer</h1>
             <h2>React State + useEffect + Public API </h2>
             <h3>The Total Posts Counts: 100 </h3>
+            </div>
         </div>
     );
 }
